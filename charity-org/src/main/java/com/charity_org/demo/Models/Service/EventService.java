77ea4fcd5e4c1b2acd7eaae89139798d857e9e1f
MostafaAlnaggar <mostafaessam202@gmail.com>
2024-11-13 13:21:20 +0200

    updating event controller

--- charity-org/src/main/java/com/charity_org/demo/Models/Service/EventService.java
+++ charity-org/src/main/java/com/charity_org/demo/Models/Service/EventService.java
@@ -1,5 +1,4 @@
 package com.charity_org.demo.Models.Service;
-
 import com.charity_org.demo.Enums.EventStatus;
 import com.charity_org.demo.Models.Address;
 import com.charity_org.demo.Models.Event;
@@ -7,10 +6,12 @@ import com.charity_org.demo.Models.repository.AddressRepository;
 import com.charity_org.demo.Models.repository.EventRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
-
 import java.util.ArrayList;
 import java.util.Date;
 import java.util.List;
+import java.util.Optional;
+import java.util.stream.Collectors;
+
 
 @Service
 public class EventService implements IEventSubject {
@@ -77,4 +78,46 @@ public class EventService implements IEventSubject {
     public List<Event> getAllEvents() {
         return eventRepository.findAll();
     }
+
+    public List<Event> listAllEvents() {
+        return eventRepository.findAll().stream()
+                .filter(event -> !event.isDeleted())
+                .collect(Collectors.toList());
+    }
+
+
+    // Update event details
+    public boolean update(Long id, String eventName, Date eventDate, int eventLocationId, String description, EventStatus status) {
+        Optional<Event> optionalEvent = eventRepository.findById(id);
+        if (optionalEvent.isPresent()) {
+            Event event = optionalEvent.get();
+            event.setEventName(eventName);
+            event.setEventDate(eventDate);
+            event.setEventLocationId(eventLocationId);
+            event.setDescription(description);
+            event.setStatus(status);
+            eventRepository.save(event);
+            return true;
+        }
+        return false;
+    }
+
+    // Soft delete event by setting isDeleted to true
+    public boolean deleteById(Long id) {
+        Optional<Event> optionalEvent = eventRepository.findById(id);
+        if (optionalEvent.isPresent()) {
+            Event event = optionalEvent.get();
+            event.setDeleted(true); // Set isDeleted to true instead of deleting the record
+            eventRepository.save(event);
+            return true;
+        }
+        return false;
+    }
+
+    // Soft delete event by setting isDeleted to true
+    public Event getById(Long id) {
+        return eventRepository.findById(id).orElse(null);
+    }
+
+
 }
