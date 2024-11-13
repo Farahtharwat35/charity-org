978595ebae9d0f76eff85c43eb071c0f4330ffb9
Salma Elsoly <salmaelsoly@gmail.com>
2024-11-13 15:21:02 +0200

    remove unneeded files
    Co-authored-by: Mariam Wahdan <mariamwahdan32@gmail.com>

--- charity-org/src/main/java/com/charity_org/demo/Models/DonationTotalPrice.java
+++
@@ -1,5 +0,0 @@
-package com.charity_org.demo.Models;
-
-public interface DonationTotalPrice {
-    public double calculatePrice();
-}
--- charity-org/src/main/java/com/charity_org/demo/Models/TotalPriceDecorator.java
+++
@@ -1,6 +0,0 @@
-package com.charity_org.demo.Models;
-
-public abstract class TotalPriceDecorator {
-    private TotalPriceDecorator donationTotalPrice;
-
-}
