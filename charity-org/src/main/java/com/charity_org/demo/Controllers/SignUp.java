90e66072d41ebe13d0f6932579e4b379e71f1995
Salma Elsoly <salmaelsoly@gmail.com>
2024-11-16 14:38:24 +0200

    refactor to use jsonProperty

--- charity-org/src/main/java/com/charity_org/demo/Controllers/SignUp.java
+++ charity-org/src/main/java/com/charity_org/demo/Controllers/SignUp.java
@@ -2,28 +2,24 @@ package com.charity_org.demo.Controllers;
 
 
 import com.charity_org.demo.DTO.SignUpRequest;
-import com.charity_org.demo.Models.User;
-import com.charity_org.demo.Models.repository.AddressRepository;
 import com.charity_org.demo.Models.Service.RolesDecorator.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.BindingResult;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
 
-import javax.validation.Valid;
+import javax.validation.groups.Default;
 
 @RestController
-@RequestMapping("/signup")
 public class SignUp {
 
     @Autowired
     private UserService userService;
 
-    @Autowired
-    private AddressRepository addressRepository;// Assume we have a UserRepository to interact with the database
 
-    @PostMapping
-    public ResponseEntity<?> signup(@RequestBody @Valid SignUpRequest signupRequest, BindingResult bindingResult) {
+    @PostMapping("/signup")
+    public ResponseEntity<Object> signup(@RequestBody @Validated({SignUpRequest.Create.class, Default.class}) SignUpRequest signupRequest, BindingResult bindingResult) {
 
         // Checking for validation errors
         if (bindingResult.hasErrors()) {
--- charity-org/src/main/java/com/charity_org/demo/DTO/SignUpRequest.java
+++ charity-org/src/main/java/com/charity_org/demo/DTO/SignUpRequest.java
@@ -1,25 +1,34 @@
 package com.charity_org.demo.DTO;
+import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.Data;
+import org.hibernate.validator.constraints.Email;
 import org.hibernate.validator.constraints.NotEmpty;
 
+import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Size;
 
 @Data
 public class SignUpRequest {
+    public interface Create {}
 
-    @NotEmpty(message = "Name is required.")
+    @NotEmpty(message = "Name is required.", groups = Create.class)
+    @JsonProperty("name")
     private String name;
 
-    @NotEmpty(message = "Address ID is required.")
+    @NotNull(message = "Address ID is required.", groups = Create.class)
+    @JsonProperty("address_id")
     private long addressId;
 
-    @NotEmpty(message = "Email is required.")
+    @NotEmpty(message = "Email is required.", groups = Create.class)
+    @JsonProperty("email")
     private String email;
 
-    @NotEmpty(message = "Password is required.")
-    @Size(min = 6, max = 20, message = "Password must be between 6 and 20 characters.")
+    @NotEmpty(message = "Password is required.", groups = Create.class)
+    @Size(min = 6, max = 20, message = "Password must be between 6 and 20 characters.", groups = Create.class)
+    @JsonProperty("password")
     private String password;
 
+    @JsonProperty("age")
     private int age;
 
 }
