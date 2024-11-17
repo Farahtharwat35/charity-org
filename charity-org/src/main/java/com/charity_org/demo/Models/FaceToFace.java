78d762178c27eb107a5d6a7eaad10a46baf56d67
s-asker <sherifasker2@gmail.com>
2024-11-17 15:41:53 +0200

    Add view of FaceToFace + Modify courier

--- charity-org/src/main/java/com/charity_org/demo/Models/FaceToFace.java
+++ charity-org/src/main/java/com/charity_org/demo/Models/FaceToFace.java
@@ -1,16 +1,21 @@
 package com.charity_org.demo.Models;
 
 import jakarta.persistence.*;
-import lombok.Data;
+import lombok.*;
 
-@Entity
+
 @Data
+@Entity
+@Setter
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
 public class FaceToFace extends BaseEntity implements IPaymentMethod{
     //@Column(nullable = false)
     //float currency;
 //    @OneToMany(mappedBy = "courier-id")
     @ManyToOne
     @JoinColumn(name = "courier-id")
-    private String courierId;
+    private Courier courier;
 
 }
