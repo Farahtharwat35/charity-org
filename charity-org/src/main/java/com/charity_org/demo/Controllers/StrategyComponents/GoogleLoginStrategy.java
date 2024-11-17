bfd9a9c7339a059988032ab42256af4cc756d0d9
Mariam Wahdan <mariamwahdan32@gmail.com>
2024-11-17 19:04:44 +0200

    added needed dependencies

--- charity-org/src/main/java/com/charity_org/demo/Controllers/StrategyComponents/GoogleLoginStrategy.java
+++ charity-org/src/main/java/com/charity_org/demo/Controllers/StrategyComponents/GoogleLoginStrategy.java
@@ -8,12 +8,9 @@ import org.springframework.stereotype.Component;
 public class GoogleLoginStrategy implements LoginStrategyInterface {
     @Override
     public boolean login(LoginRequest loginRequest) {
-        // Simulate authentication logic for Google
-        if (loginRequest.getToken() != null && !loginRequest.getToken().isEmpty()) {
-            // Mock token validation (replace with actual validation)
+
             return true; // Assuming token is valid
-        }
-        return false; // If no token is provided or token is invalid
+       // If no token is provided or token is invalid
     }
 }
 
