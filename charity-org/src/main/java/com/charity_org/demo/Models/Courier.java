e4d7f6aad13edc91e0faafe8ed9d3651e4b509a3
s-asker <sherifasker2@gmail.com>
2024-11-17 13:46:10 +0200

    Add FacetoFace

--- charity-org/src/main/java/com/charity_org/demo/Models/Courier.java
+++ charity-org/src/main/java/com/charity_org/demo/Models/Courier.java
@@ -1,25 +1,18 @@
-//package com.charity_org.demo.Models;
-//
-//import jakarta.persistence.Entity;
-//import jakarta.persistence.JoinColumn;
-//import jakarta.persistence.ManyToOne;
-//import lombok.Data;
-//
-//@Data
-//@Entity
-//public class Courier extends Person {
-//
-//    @ManyToOne
-//    @JoinColumn(name = "userId")
-//    private String phoneNumber;
-//
-//   /* @Override
-//    public void processPayment() {
-//
-//    }
-//
-//    @Override
-//    public void Confirmation() {
-//
-//    } */
-//}
+package com.charity_org.demo.Models;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.OneToMany;
+import lombok.Data;
+
+import java.util.List;
+
+@Data
+@Entity
+public class Courier extends Person {
+
+    @OneToMany(mappedBy = "facetoface")
+    List<FaceToFace> ordersList;
+
+}
--- charity-org/src/main/java/com/charity_org/demo/Models/FaceToFace.java
+++ charity-org/src/main/java/com/charity_org/demo/Models/FaceToFace.java
@@ -1,9 +1,16 @@
 package com.charity_org.demo.Models;
 
-import jakarta.persistence.Column;
+import jakarta.persistence.*;
+import lombok.Data;
 
+@Entity
+@Data
 public class FaceToFace extends BaseEntity implements IPaymentMethod{
-    @Column(nullable = false)
-    float currency;
+    //@Column(nullable = false)
+    //float currency;
+//    @OneToMany(mappedBy = "courier-id")
+    @ManyToOne
+    @JoinColumn(name = "courier-id")
+    private String courierId;
 
 }
